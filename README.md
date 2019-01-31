64c5ad8f740aa243b546019ee4c3099f4ee84eaa
Bondi007 <bondar.tamas@gmail.com>
2019-01-31 20:49:15 +0100

    - First test with Chrome

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Selenium_WebDriverManager
 Trying WebDriverManager
+
---
+++ src/test/java/ChromeTest.java
@@ -0,0 +1,47 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
+
+/**
+ * Created by Bondár Tamás on 2019. 01. 31..
+ */
+public class ChromeTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setupClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        By searchInput = By.id("searchInput");
+        wait.until(presenceOfElementLocated(searchInput));
+        driver.findElement(searchInput).sendKeys("Software");
+        By searchButton = By.id("searchButton");
+        wait.until(elementToBeClickable(searchButton));
+        driver.findElement(searchButton).click();
+
+        wait.until(textToBePresentInElementLocated(By.tagName("body"),
+                "Computer software"));
+    }
+}
